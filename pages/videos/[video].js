2216fe9d76aff43a049e269b0f0f199f0ce77c27
BaoNgo88 <tngo67@gatech.edu>
2023-03-04 22:44:48 -0500

    finish message feature

--- pages/videos/[video].js
+++ pages/videos/[video].js
@@ -3,43 +3,66 @@ import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import { Box, Grid, Stack } from '@mui/system'
 import { TextField } from '@mui/material'
+import socketIOClient from "socket.io-client"
+
 
 export default function Video() {
+
+    // socket instance
+    const socket = socketIOClient(`localhost:3001`, {secure: false});
     const router = useRouter()
-    // const id = router.query.video
+    const user_info = router.query
 
-    const [curMsg, setCurMsg] = useState("Type your message here");
+    useEffect(() => {
+        // Our path: http://localhost:3000/videos/meeting?name=Aryan+Patel&id=191952
+        console.log(user_info)
+        socket.emit("info", user_info)
+    }, [])
 
+    // messages states
+    const [curMsg, setCurMsg] = useState("")
+    const [sent, setSent] = useState("false")
     const [textMessages, setTextMessages] = useState([
         {
-            msg: "Hello There!",
-            from: 1
+            name: "Bot1",
+            msg: "Hello Bot2!"
         },
         {
-            msg: "Hello Steve!",
-            from: 0
-        },
-        {
-            msg: "Hello There!",
-            from: 1
-        },
-        {
-            msg: "Hello Steve!",
-            from: 1
+            name: "Bot2",
+            msg: "Hello Bot1!"
         }
     ]);
 
     const submitMsg = (e) => {
-        if (e.keyCode != 13)
-            return
-        console.log(e.target.value)
-        textMessages.push({
-            msg: e.target.value,
-            from: 1
-        })
-        setCurMsg("Type your message here")
-        console.log(textMessages)
+        if (e.keyCode == 13 && e.target.value != "") {
+            setSent("true")
+            console.log(e.target.value)
+        }
     }
+
+    // Run whenever sent is true
+    useEffect(() => {
+        if (sent === "true") {
+            // Prepare to send message to all other in the room
+            socket.emit("send_message", {
+                name: user_info['name'],
+                msg: curMsg,
+                id: user_info['id']
+            })
+            console.log("I am sending message" + curMsg)
+        }
+
+        // Retrieve message from other individuals
+        socket.on("from_message", (incomingMsg) => {
+            console.log("I am getting an message" + incomingMsg['msg'])
+            const newTextMessagesArray = textMessages
+            newTextMessagesArray.push(incomingMsg)
+            setTextMessages([...newTextMessagesArray])
+        })
+        setCurMsg("")
+        setSent("false")
+
+    }, [sent])
 
     return (
         <div className='videoPage'>
@@ -50,20 +73,20 @@ export default function Video() {
                 <Stack spacing={1} className="chatStack">
                     {textMessages.map((cur, ind) => {
                         return (
-                            <TextBubble key={ind} msg={cur['msg']} col={cur['from']} />
+                            <TextBubble key={ind} msg={cur['msg']} name={cur['name']}/>
                         )
                     })}
                     <Stack spacing={10} className="bottomStack">
                         <div></div>
                         <TextField
-                        label=""
-                        type="Outlined"
-                        autoComplete="current-password"
-                        className='chatBar'
-                        value={curMsg}
-                        onChange={e=>{setCurMsg(e.target.value)}}
-                        onKeyDown={e => {submitMsg(e)}}
-                    />
+                            label=""
+                            type="Outlined"
+                            autoComplete="current-password"
+                            className='chatBar'
+                            value={curMsg}
+                            onChange={e=>{setCurMsg(e.target.value)}}
+                            onKeyDown={e => {submitMsg(e)}}
+                        />
                     </Stack>
                 </Stack>
             </div>
@@ -74,7 +97,7 @@ export default function Video() {
 const TextBubble = (props) => {
     return (
         <Box className={`TextMessageBox ${props.col == 1 ? "msgMe" : "msgThem"}`}>
-            {props.msg}
+            {props.name}: {props.msg}
         </Box>
     )
 }
--- server.js
+++ server.js
@@ -8,20 +8,41 @@ app.use(cors());
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",//"*",
+      origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     }
   });
-  
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-  });
+// app.get('/eatfoo/steve', (req, res) => {
+//   res.send("Steve is eating")
+// })
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello world</h1>');
+//   });
   
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected to room');
+
+  // Join the user to roomID
+  socket.on("info", (data) => {
+    console.log(data)
+    socket.join(data.id)
+  })
+
+  // Receiving messages from clients
+  socket.on("send_message", (data) => {
+    console.log(data)
+    io.in(data['id']).emit("from_message", data)
+  })
+
+  socket.on('disconnect', (socket) => {
+    console.log(`A disconnected user`);
+  });
 });
+
+
 
 server.listen(3001, () => {
   console.log('listening on *:3001');
