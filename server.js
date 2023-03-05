94f7b0c9d8be321a60eb97ed8fb20322502cdbe8
BaoNgo88 <tngo67@gatech.edu>
2023-03-04 20:30:29 -0500

    new server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const cors = require('cors'); 
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",//"*",
+      methods: ["GET", "POST"],
+    }
+  });
+  
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+  });
+  
+
+io.on('connection', (socket) => {
+  socket.join("asfjdjaf")
+
+  console.log('a user connected');
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
