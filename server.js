120910d8ede9ce0ba2279c88730e4fb4d73c1411
Aryan Patel <patelaryan1013@gmail.com>
2023-08-02 15:19:03 -0400

    uncommented peer import

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require('cors'); 
-// const { PeerServer } = require("peer");
+ const { PeerServer } = require("peer");
 app.use(cors());
 
 
